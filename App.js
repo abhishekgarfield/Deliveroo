c69a84af792d9df77c47caab2b464ae4bea941a9
abhishekgarfield <abhishek2759@gmail.com>
2022-08-24 18:13:24 +0530

    start

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text>Universal React with Expo</Text>
+      <Text>Universal Rt with Expo</Text>
     </View>
   );
 }
