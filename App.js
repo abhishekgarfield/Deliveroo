a6a9b992c946565dff2f4443f9bd095fa73a5073
abhishekgarfield <abhishek2759@gmail.com>
2022-08-31 21:32:37 +0530

    restaurabt screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { View, Text ,StyleSheet } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Homescreen from "./screens/Homescreen";
+import Restaurantscreen from "./screens/Restaurantscreen"
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="home" component={Homescreen} />
+        <Stack.Screen name="restaurant" component={Restaurantscreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/Restaurantscreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+
+const Restaurantscreen=()=>{
+    return(
+        <View>
+            <Text>Restaurant screen</Text>
+        </View>
+    );
+}
+
+export default Restaurantscreen;
