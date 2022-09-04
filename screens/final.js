1e0b8ea065f33e23337537823ff4a81428e53c9a
abhishekgarfield <abhishek2759@gmail.com>
2022-09-04 20:52:45 +0530

    sdf

--- screens/final.js
+++ screens/final.js
@@ -1,11 +1,30 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { XCircleIcon, XMarkIcon } from "react-native-heroicons/solid";
+import { useSelector } from "react-redux";
+import {selectRestaurant} from "../fetures/restaurantSlice"
 
-const Finalscreen=()=>{
-    return(<>
-    <View>
-        <Text>hi</Text>
-    </View>
-    </>);
-}
+const Finalscreen = () => {
+    const reataurantname=useSelector((state)=>{return state.restaurant.restaurant.title})
+  return (
+    <>
+      <View style={{flexGrow:1,backgroundColor:"rgba(0,204,188,255)"}}>
+        <SafeAreaView>
+            <View style={{flexDirection:"row",justifyContent:"space-between",marginHorizontal:10,alignItems:"center"}}>
+                <TouchableOpacity>
+                    <XMarkIcon size={20} color="white"/>
+                </TouchableOpacity>
+                <Text style={{color:"white",fontSize:20,fontWeight:"400"}}>Need help ?</Text>
+            </View>
+            <View>
+                <Text>Estimated arrival</Text>
+                <Text>45-50 Minutes</Text>
+                <Text>{`Your order at ${reataurantname} is being prepared`}</Text>
+            </View>
+        </SafeAreaView>
+      </View>
+    </>
+  );
+};
 
 export default Finalscreen;
